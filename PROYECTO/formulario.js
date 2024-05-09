088f5416b915b826054286503c41571b75cbe1f1
SALA 406D <s406d@umanizales.loc>
2024-05-08 19:53:27 -0500

    Mi primer commit

---
+++ PROYECTO/formulario.js
@@ -0,0 +1,12 @@
+const formulario = document.getElementById('formulario')
+
+formulario.addEventListener('submit'), function(){
+    const name = document.getElementById(name).value
+    const lastname = document.getElementById(lastname).value
+    const identificacion = document.getElementById(identificacion).value
+    const email = document.getElementById(email).value
+    const Direccion = document.getElementById(Direccion).value
+    const numero = document.getElementById(Telefono).value
+    alert('sus datos se guardaron exitosamente')
+}
+
